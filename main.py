1ef1fe9c26eacf39a6feace8ad4fe0015793a190
Underbar-Git <86478519+Underbar-Git@users.noreply.github.com>
2023-09-05 15:24:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ Hello World!
 res = requests.get('https://api.open-meteo.com/v1/forecast?latitude=37.566&longitude=126.9784&hourly=temperature_2m&past_days=2&forecast_days=3')
 st.json(res.json())
 
-
+df = pd.DataFrame(res.json(), columns=['time', 'temperature_2m'])
+st.dataframe(df)
 
 redirect_button("https://toss.me/underbars","후원 감사합니다 🩵")
