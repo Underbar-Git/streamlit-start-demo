edc896a123dff185c60342930d9c4f9a9a03076f
Underbar-Git <86478519+Underbar-Git@users.noreply.github.com>
2023-09-05 14:23:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 import streamlit as st
 
+def redirect_button(url: str, text: str= None, color="#FD504D"):
+    st.markdown(
+    f"""
+    <a href="{url}" target="_blank">
+        <div style="
+            display: inline-block;
+            padding: 0.5em 1em;
+            color: #FFFFFF;
+            background-color: {color};
+            border-radius: 3px;
+            text-decoration: none;">
+            {text}
+        </div>
+    </a>
+    """,
+    unsafe_allow_html=True
+    )
+    
 st.write('''
 # Streamlit Start Demo
 Hello World!
 ''')
+
+
+redirect_button("https://toss.me/underbars","후원은 사랑입니다 ❤️")
