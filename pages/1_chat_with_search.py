1cf52edfe2b08e2f01ec4267486f373cc01820af
황인건 <hwang@hiconsy.com>
2024-01-16 16:39:35 +0900

    잼민이 추가

--- pages/1_chat_with_search.py
+++ pages/1_chat_with_search.py
@@ -1,12 +1,35 @@
 import streamlit as st
 from streamlit_chat import message 
 from duckduckgo_search import DDGS
+from langchain.schema.document import Document
+from langchain.chains import RetrievalQA
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
 import requests
+GOOGLE_API_KEY = "AIzaSyA10EDo80sAY2XoQsY_KoU_IgbBdcKX5yg"
+from langchain_google_genai import ChatGoogleGenerativeAI
+from google import generativeai as genai
+from langchain.chains import ConversationalRetrievalChain
+from langchain.prompts import PromptTemplate
 
+model_name = "jhgan/ko-sbert-nli"
+model_kwargs = {'device': 'mps'}
+encode_kwargs = {'normalize_embeddings': True} # set True to compute cosine similarity
+embeddings = HuggingFaceEmbeddings(
+    model_name=model_name,
+    model_kwargs=model_kwargs,
+    encode_kwargs=encode_kwargs
+)
+custom_prompt_template = """
+{context}
+Question: {question}
+Helpful Answer:
+"""
 
 def web_search(q):
     with DDGS() as ddgs:
-        results = [r for r in ddgs.text(q, max_results=5)]
+        results = [r for r in ddgs.text(q, region="ko-ko", max_results=1000)]
     return results
 
 if 'generated' not in st.session_state:
@@ -32,10 +55,57 @@ user_input = get_text()
 def generate_response(prompt):
     response = web_search(prompt)
     
-    res = ""
-    if response:
-        for r in response:
-            res += '<a href="' + r['href'] + '" target="_blank">' + r['title'] + '</a>\n\n' + r['body'] + '\n\n\n'
+    documents = []
+    for r in response:
+        document = Document(
+            page_content=r['title'] + '\n' + r['body'],
+            metadata={'source': r['href']}
+        )
+        documents.append(document)
+    
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    texts = text_splitter.split_documents(documents)
+    # print(texts)
+    
+    store = FAISS.from_documents(texts, embeddings)
+    retriever = store.as_retriever()
+    
+    safety_settings_NONE=[
+        { "category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_NONE" },
+        { "category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_NONE" },
+        { "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_NONE" },
+        { "category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_NONE"}
+    ]
+    
+    LLM = ChatGoogleGenerativeAI(
+        model="gemini-pro", 
+        temperature=0,
+        max_output_tokens=2048,
+        google_api_key=GOOGLE_API_KEY)
+    LLM.client = genai.GenerativeModel(
+        model_name="gemini-pro", 
+        safety_settings=safety_settings_NONE
+    )
+    print('LLM?', LLM)
+    custom_prompt = PromptTemplate(template=custom_prompt_template, input_variables=["context", "question"])
+    
+    chain = RetrievalQA.from_chain_type(
+        llm=LLM,
+        chain_type="stuff",
+        retriever=retriever,
+        verbose=True,
+        return_source_documents=True,
+        chain_type_kwargs={"prompt": custom_prompt},
+    )
+    # print('chain', chain)
+    q = {'query': prompt}
+    results = chain(q)
+    print('result :::', results)
+    res = results['result']
+    # res = ""
+    # if response:
+    #     for r in response:
+    #         res += '<a href="' + r['href'] + '" target="_blank">' + r['title'] + '</a>\n\n' + r['body'] + '\n\n\n'
     
     if res == "":
         res = "모르겠는데?"
